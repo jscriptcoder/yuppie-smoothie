d67a5477a7a97ecbb6ed5f0e8742fb4070ebaf23
Fran <jscriptcoder@gmail.com>
2016-07-17 21:54:15 +0200

    drag&dop works ongoing

--- src/app/recipes/recipe/recipe.ts
+++ src/app/recipes/recipe/recipe.ts
@@ -3,16 +3,20 @@ import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_ICON_DIRECTIVES } from '@angular2-material/icon';
 import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+import { MD_GRID_LIST_DIRECTIVES } from '@angular2-material/grid-list';
+import { DND_PROVIDERS, DND_DIRECTIVES } from 'ng2-dnd/ng2-dnd';
 import { BackendService, Product } from '../../../backend/backend';
 
 @Component({
   selector: 'recipe',
-  providers: [ BackendService ],
+  providers: [ DND_PROVIDERS, BackendService ],
   directives: [
   	MD_CARD_DIRECTIVES,
     MD_LIST_DIRECTIVES,
     MD_ICON_DIRECTIVES,
     MD_INPUT_DIRECTIVES,
+    MD_GRID_LIST_DIRECTIVES,
+    DND_DIRECTIVES,
   ],
   styleUrls: ['./recipe.scss'],
   templateUrl: './recipe.html'
